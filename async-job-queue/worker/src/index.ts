7ccac813b0af761180abfc4ffaf2d22c45e18e0b
Naman13112004 <njudaipur04@gmail.com>
2026-03-13 17:16:34 +0530

    added worker to simulate email sending

---
+++ async-job-queue/worker/src/index.ts
@@ -0,0 +1,19 @@
+import { Job, Worker } from "bullmq";
+import { delay } from "./util.js";
+
+const worker = new Worker("emailQueue", async (job: Job) => {
+    const { email, message } = job.data;
+
+    console.log(`Sending email to ${email}`);
+    await delay(10*1000);
+    console.log({
+        email: email,
+        message: message,
+    });
+    console.log(`Email sent to ${email}`);
+}, {
+    connection: {
+        host: "localhost",
+        port: 6379,
+    }
+})
